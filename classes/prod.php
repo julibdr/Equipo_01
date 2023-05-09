6ab4407502781d5aabadc03c9bc7e394601c635b
ailenad <ailen.alvarez@davinci.edu.ar>
2023-05-09 17:10:57 -0300

    Clases
    Carpetas clases

---
+++ classes/prod.php
@@ -0,0 +1,33 @@
+<?php
+class Celulares {
+    protected $id;
+    protected $nombre;
+    protected $descripcion;
+    protected $precio;
+    protected $imagen;
+    protected $categoria;
+    protected $pantalla;
+    protected $sistemaoperativo;
+}
+
+class Ipad {
+    protected $id;
+    protected $nombre;
+    protected $descripcion;
+    protected $precio;
+    protected $imagen;
+    protected $categoria;
+    protected $pantalla;
+    protected $sistemaoperativo;
+}
+class Mac {
+    protected $id;
+    protected $nombre;
+    protected $descripcion;
+    protected $precio;
+    protected $imagen;
+    protected $categoria;
+    protected $pantalla;
+    protected $sistemaoperativo;
+
+}
