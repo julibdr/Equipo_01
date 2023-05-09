738d72b0a854804a92d5a1bbfaccb7068a44180f
ailenad <ailen.alvarez@davinci.edu.ar>
2023-05-08 22:08:43 -0300

    Mejoras
    estructua de carpetas, contenido dinamico del nav y clase

---
+++ partials/footer.php
@@ -0,0 +1,13 @@
+
+
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+    <img src="" alt="">
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="index.php?seccion=productos" class="nav-link px-2 text-muted">Productos</a></li>
+      <li class="nav-item"><a href="index.php?seccion=sobre_nosotros" class="nav-link px-2 text-muted">Sobre nosotros</a></li>
+      <li class="nav-item"><a href="" class="nav-link px-2 text-muted">Redes</a></li>
+    </ul>
+  </footer>
+  </body>
+</html>
