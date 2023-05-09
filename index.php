e5985e2fac1717f507dfa2fa9d7b2e206beaa59e
ailenad <ailen.alvarez@davinci.edu.ar>
2023-05-09 12:37:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-=======
+
 <?php
     $vista = $_GET['seccion'];
     $vista = $vista ? $vista : 'home';
@@ -22,4 +9,4 @@
 <?php include ("views/$vista.php")?>
 <?php include('partials/footer.php') ?>
 
->>>>>>> Stashed changes
+
