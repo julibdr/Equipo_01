5e1c2c57d7420626e96fe9a87fe3e8e3518cb0bb
julieta bariandaran <julietabdr@icloud.com>
2023-05-10 18:38:42 -0300

    lio
    arreglando el lio que hice :(

--- views/contacto.php
+++ views/contacto.php
@@ -1,2 +1,29 @@
 <main>
 <div class="col-6 mx-auto p-6">
+<main>
+<div class="col-6 mx-auto p-6">
+<form action="procesar_datos.php" method="POST">
+    <h1>Contactate con nosotros</h1>
+  <div class="col-md-6">
+    <label for="exampleInputEmail1" class="form-label">Nombre</label>
+    <input type="nombre" name="nombre" class="form-control" id="nombre" aria-describedby="nombre">
+    <div id="nombre" class="form-text">Escriba su nombre.</div>
+  </div>
+  <div class="col-md-6">
+    <label for="exampleInputEmail1" class="form-label">Apellido</label>
+    <input type="apellido" name="apellido" class="form-control" id="apellido" aria-describedby="apellido">
+    <div id="apellido" class="form-text">Escriba su apellido.</div>
+  </div>
+  <div class="col-md-6">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" name="email" class="form-control" id="Email" aria-describedby="email">
+    <div id="email" class="form-text">Escriba su email.</div>
+  </div>
+  <div class="col-md-6">
+  <label for="exampleFormControlTextarea1" class="form-label">Consulta</label>
+  <textarea class="form-control" type="consulta" name="consulta" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+</div>
+</main>
--- views/sobreNosotros.php
+++ views/sobreNosotros.php
@@ -1,3 +1,34 @@
+<?php
+// (void) -> string
+require_once "classes/alumnos.php";
 
 
+function leerAlumno() {
+    $content = file_get_contents("data/alumnos.json"); //string
+    $contentConversion = json_decode($content, true); //object
+    return $contentConversion["alumnos"];
+}
 
+function crearAlumno($list): string {
+    //return json_encode($list);
+    $html = "";
+    $html .= "<div class='row row-cols-2 m-4 g-3'>";
+    foreach($list as $item) {
+        $html .= "<div class='card g-3 m-3' style='width: 18rem'>";
+        $html .= "<img src='{$item['imagen']}' class='card-img-top' alt='Nombre'>";
+        $html .= "  <div class='card-body'>";
+        $html .= "<h2 class='card-title-nosotros'>{$item['nombre']}</h2>";
+        $html .= "<p class='card-text'>{$item['edad']}</p>";
+        $html .= "<p class='card-text-correo'>{$item['correo']}</p>";
+        $html .= "<p class='card-text'>{$item['redes']}</p>";
+        $html .= "</div>";
+        $html .= "</div>";
+
+    }
+    $html .= "</div>";
+    return $html;
+}
+
+?>
+
+<?= crearAlumno(leerAlumno()) ?> 
