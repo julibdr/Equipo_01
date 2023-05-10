e4f07474bfa7b1a262a4add320152d85e611e670
ailenad <ailen.alvarez@davinci.edu.ar>
2023-05-10 18:01:51 -0300

    Formulario

---
+++ procesar_datos.php
@@ -0,0 +1,9 @@
+<?php 
+$nombre= $_POST['nombre'];
+$apellido= $_POST['apellido'];
+$email= $_POST['email'];
+$consulta= $_POST['consulta'];
+// $submit= $_POST['submit'];
+
+echo "<p>Esto es" .$nombre ." </p>";
+?>
