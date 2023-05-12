956267a2177bdeadf717268bd366463f267406ef
julieta bariandaran <julietabdr@icloud.com>
2023-05-12 16:58:54 -0300

    form
    mostrar datos del form

--- procesar_datos.php
+++ procesar_datos.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 $nombre= $_POST['nombre'];
 $apellido= $_POST['apellido'];
 $email= $_POST['email'];
@@ -6,4 +6,31 @@ $consulta= $_POST['consulta'];
 // $submit= $_POST['submit'];
 
 echo "<p>Esto es" .$nombre."</p>";
+?> -->
+
+<?PHP
+    // LOS VALORES ENVIADOS POR GET VAN A FIGURAR EN LA VARIABLE SUPERGLOBAL $_GET
+    // echo "<pre>";
+    // print_r($_GET);
+    // echo "</pre>";
+
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $consulta = $_POST['consulta'];
+
+    if(!$nombre) echo "El campo.....";
+
 ?>
+
+
+<div class="card" style="width: 18rem;">
+<ul class="list-group list-group-flush">
+<li class="list-group-item">El nombre es: <?= $nombre ?></li>
+<li class="list-group-item">El apellido es: <?= $apellido ?></li>
+<li class="list-group-item">El email es: <?= $email ?></li>
+<li class="list-group-item">La consulta es: <?= $consulta ?></li>
+</ul>
+</div>
+
+
