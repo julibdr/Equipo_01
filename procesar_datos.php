b2fc80dd4020d4ee86a894a82145ec24b86cc823
julieta bariandaran <julietabdr@icloud.com>
2023-05-18 20:47:14 -0300

    form
    estilizar el css

--- procesar_datos.php
+++ procesar_datos.php
@@ -1,3 +1,8 @@
+
+<main>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</main>
+
 <!-- <?php 
 $nombre= $_POST['nombre'];
 $apellido= $_POST['apellido'];
@@ -23,14 +28,16 @@ echo "<p>Esto es" .$nombre."</p>";
 
 ?>
 
-
-<div class="card" style="width: 18rem;">
+<div class="row justify-content-center mt-5">
+<div class="card" style="width: 20rem;">
+<h1></h1>
 <ul class="list-group list-group-flush">
 <li class="list-group-item">El nombre es: <?= $nombre ?></li>
 <li class="list-group-item">El apellido es: <?= $apellido ?></li>
 <li class="list-group-item">El email es: <?= $email ?></li>
 <li class="list-group-item">La consulta es: <?= $consulta ?></li>
 </ul>
+</div>
 </div>
 
 
