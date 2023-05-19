40d002d18a78f565ba702a9b9e2e17c9d2540ae3
Luli <lulimar2002@hotmail.com>
2023-05-19 11:26:13 -0300

    procesar_datos.php
    include

--- procesar_datos.php
+++ procesar_datos.php
@@ -1,7 +1,4 @@
-
-<main>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</main>
+<?php include('partials/header.php')?>
 
 <!-- <?php 
 $nombre= $_POST['nombre'];
@@ -39,5 +36,7 @@ echo "<p>Esto es" .$nombre."</p>";
 </ul>
 </div>
 </div>
+
+<?php include('partials/footer.php')?>
 
 
