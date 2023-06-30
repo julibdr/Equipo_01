67b8c6f2192b9d2a4e020b331db2af523e7f345f
julieta bariandaran <julietabdr@icloud.com>
2023-06-30 18:43:48 -0300

    login

--- autenticacion.php
+++ autenticacion.php
@@ -1,15 +1,23 @@
 <?php
 session_start();
-
+require_once "registro.php";
 require_once "classes/AutenticacionesClass.php";
 require_once "classes/Connection.php";
 require_once "classes/Usuario.php";
+
 
 $postData = $_POST;
 
 $login = ((new classes\autenticacion())->logIn($postData["email"], $postData["password"]));
 
 
+
+
+if($registro){
+    header('location: index.php?seccion=home');
+} else {
+    header('location: index.php?seccion=addUserView');
+};
 if($login) {
     header('location: index.php?seccion=home');
 } else {
--- registro.php
+++ registro.php
@@ -1,13 +1,27 @@
 <?php
-session_start();
+
 
 require_once "classes/RegistroClass.php";
 require_once "classes/Connection.php";
 require_once "classes/Usuario.php";
+require_once "autenticacion.php";
 
 $postData = $_POST;
 
-$registro = new Usuario();
+
+
+try {
+    $registro = (new \classes\Usuario());
+
+    $registro->add(
+        $postData['email'],
+        $postData['password']
+    );
+
+    header("Location: ../index.php");
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
 
 
 if($registro){
